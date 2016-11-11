72a0e668af8e4bad33c981aa966cf343fd74ca39
rarimoto <masae1263@gmail.com>
2016-11-11 02:24:22 -1000

    got toggle working, not sliding tho

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,11 @@
-$(document).ready(function () {
   var $navToggle = $('.header_mobile_toggle-img');
-  var $dropmenu = $('.toggle_nav');
+  var $dropmenu = $('.toggle');
   var $window = $(window);
   var mobileBreakpoint = 1024;
 
-  $navToggle.on('click', function() {
+$(document).ready(function() {
+  $navToggle.on('click', function(e) {
+    e.preventDefault();
     $dropmenu.slideToggle();
     $navToggle.toggleClass('active');
   });
